c16b5ce9d0b39ce3d627698d0b4b3149d750f556
Nishant kumar <nishantkr2003nna@gmail.com>
2025-02-18 21:33:47 +0530

    Bootstrap
    Bootstrap

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -1,7 +1,18 @@
-function App() {
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
 
-    
-  return <div>placeHolder</div>;
+function App() {
+  return (
+    <center class="todo-container">
+      <AppName />
+      <AddTodo />
+      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
+    </center>
+  );
 }
 
 export default App;
---
+++ Projects/1-todo-app-version-one/src/components/TodoItem1.jsx
@@ -0,0 +1,21 @@
+function TodoItem1() {
+  let todoName = "Buy milk";
+  let todoDate = "04/10/2024";
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">{todoName}</div>
+
+        <div class="col-4">{todoDate}</div>
+
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Danger
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
