eec6a3fd4e95f23882d3104961d6427a01fcf8b6
Nishant kumar <nishantkr2003nna@gmail.com>
2025-03-01 12:13:08 +0530

    Calculator

--- Projects/4-Calculator-version-one/calculator/src/App.jsx
+++ Projects/4-Calculator-version-one/calculator/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className={styles.calculator}>
+      <Display></Display>
+      <ButtonsContainer></ButtonsContainer>
+    </div>
+  );
 }
 
 export default App;
